dbce47cf514660f332fc45d42c0e7aa0b38c2b75
Japan Parikh <parikh.japan30@gmail.com>
2019-02-05 16:34:53 -0800

    front-end program

---
+++ OrderMeal/OrderMeal/CheckOutPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace OrderMeal
+{
+    public partial class CheckOutPage : ContentPage
+    {
+
+        public CheckOutPage(string optionOneMeals, string optionTwoMeals)
+        {
+            InitializeComponent();
+            Option1Count.Text = optionOneMeals;
+            Option2Count.Text = optionTwoMeals;
+            var totalMealOptionOnePrice = Convert.ToInt32(optionOneMeals) * 20;
+            var totalMealOptionTwoPrice = Convert.ToInt32(optionTwoMeals) * 24;
+            var tax = (totalMealOptionOnePrice + totalMealOptionTwoPrice) * 0.09;
+            var totalPrice = totalMealOptionOnePrice + totalMealOptionTwoPrice + tax;
+            Tax.Text = "$" + tax.ToString();
+            TotalPrice.Text = "$" + totalPrice.ToString();
+
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //var client = HttpClient();
+        }
+    }
+}
---
+++ OrderMeal/OrderMeal/SelectMealOptions.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace OrderMeal
+{
+    public partial class SelectMealOptions : ContentPage
+    {
+        public SelectMealOptions()
+        {
+            InitializeComponent();
+
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var secondPage = new CheckOutPage(optionOne.Text, optionTwo.Text);
+            await Navigation.PushAsync(secondPage);
+        }
+    }
+}
