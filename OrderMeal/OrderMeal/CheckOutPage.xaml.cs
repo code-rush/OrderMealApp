d0f20779e18cdf41f0a0675ae017b1a97ae3a430
amanag96 <amanag96@gmail.com>
2019-02-19 12:22:38 -0800

    Saving user data locally and pre-filling for next visit

--- OrderMeal/OrderMeal/CheckOutPage.xaml.cs
+++ OrderMeal/OrderMeal/CheckOutPage.xaml.cs
@@ -20,10 +20,28 @@ namespace OrderMeal
             Tax.Text = "$" + tax.ToString();
             TotalPrice.Text = "$" + totalPrice.ToString();
 
+            if (Application.Current.Properties.ContainsKey("user_id")) { fullName.Text = Application.Current.Properties["user_id"].ToString(); }
+            if (Application.Current.Properties.ContainsKey("email")) { emailAddress.Text = Application.Current.Properties["email"].ToString(); }
+            if (Application.Current.Properties.ContainsKey("phone")) { phoneNumber.Text = Application.Current.Properties["phone"].ToString(); }
+            if (Application.Current.Properties.ContainsKey("street")) { streetAddress.Text = Application.Current.Properties["street"].ToString(); }
+            if (Application.Current.Properties.ContainsKey("city")) { City.Text = Application.Current.Properties["city"].ToString(); }
+            if (Application.Current.Properties.ContainsKey("state")) { State.Text = Application.Current.Properties["state"].ToString(); }
+            if (Application.Current.Properties.ContainsKey("zip")) { zipCode.Text = Application.Current.Properties["zip"].ToString(); }
         }
 
         void Handle_Clicked(object sender, System.EventArgs e)
         {
+            if (fullName.Text != "") { Application.Current.Properties["user_id"] = fullName.Text; }
+
+            System.Diagnostics.Debug.Print(Application.Current.Properties["user_id"].ToString());
+
+            if (emailAddress.Text != "") { Application.Current.Properties["email"] = emailAddress.Text; }
+            if (phoneNumber.Text != "") { Application.Current.Properties["phone"] = phoneNumber.Text; }
+            if (streetAddress.Text != "") { Application.Current.Properties["street"] = streetAddress.Text; }
+            if (City.Text != "") { Application.Current.Properties["city"] = City.Text; }
+            if (State.Text != "") { Application.Current.Properties["state"] = State.Text; }
+            if (zipCode.Text != "") { Application.Current.Properties["zip"] = zipCode.Text; }
+            Application.Current.SavePropertiesAsync();
             //var client = HttpClient();
         }
     }
