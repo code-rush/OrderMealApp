ed5b9d6f1d6d07ac43b393370c8f4bda3413d59b
Japan Parikh <parikh.japan30@gmail.com>
2019-03-14 18:55:39 -0700

    requirements - add flask-mail lib

--- OrderMealBackend/requirements.py
+++ OrderMealBackend/requirements.py
@@ -2,8 +2,9 @@
 # @Author: Japan Parikh
 # @Date:   2019-02-16 17:04:34
 # @Last Modified by:   Japan Parikh
-# @Last Modified time: 2019-02-19 20:43:01
+# @Last Modified time: 2019-03-14 16:34:09
 
 flask
 flask-restful
 flask_cors
+Flask-Mail
