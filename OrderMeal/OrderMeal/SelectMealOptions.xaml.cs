c3e9ac292124dd4013ed68665d29a98ed9c73923
Japan Parikh <parikh.japan30@gmail.com>
2019-03-15 17:06:23 -0700

    update first screen to improve selecting number of meal orders

--- OrderMeal/OrderMeal/SelectMealOptions.xaml.cs
+++ OrderMeal/OrderMeal/SelectMealOptions.xaml.cs
@@ -7,26 +7,101 @@ using Xamarin.Forms;
 
 namespace OrderMeal
 {
+
     public partial class SelectMealOptions : ContentPage
     {
+        private int optionOneMealOrders = 0;
+        private int optionTwoMealOrders = 0;
+
         public SelectMealOptions()
         {
             InitializeComponent();
+
+            optionOneLabel.Text = optionOneMealOrders.ToString();
+            optionTwoLabel.Text = optionTwoMealOrders.ToString();
 
             getimg();
         }
 
         async void Handle_Clicked(object sender, System.EventArgs e)
         {
-            if (string.IsNullOrEmpty(optionOne.Text) && string.IsNullOrEmpty(optionTwo.Text)) 
+            if (optionOneMealOrders == 0 && optionTwoMealOrders == 0) 
             {
                 await DisplayAlert("Order Error", "Please make an order to continue", "Continue");
             }
             else
             {
-                var secondPage = new CheckOutPage(optionOne.Text, optionTwo.Text);
+                var secondPage = new CheckOutPage(optionOneMealOrders.ToString(), optionTwoMealOrders.ToString());
                 await Navigation.PushAsync(secondPage);
             }
+        }
+
+        private void reduceOrdersForMealOptionOne(object sender, System.EventArgs e)
+        {
+            if (optionOneMealOrders > 0)
+            {
+                optionOneMealOrders = optionOneMealOrders - 1;
+                optioneOneSlider.Value = (double)optionOneMealOrders;
+                optionOneLabel.Text = optionOneMealOrders.ToString();
+            }
+            else
+            {
+                optionOneLabel.Text = optionOneMealOrders.ToString();
+            }
+        }
+
+        private void addOrdersForMealOptionOne(object sender, System.EventArgs e)
+        {
+            if (optionOneMealOrders < 10)
+            {
+                optionOneMealOrders = optionOneMealOrders + 1;
+                optioneOneSlider.Value = (double)optionOneMealOrders;
+                optionOneLabel.Text = optionOneMealOrders.ToString();
+            }
+            else
+            {
+                optionOneLabel.Text = optionOneMealOrders.ToString();
+            }
+        }
+
+        private void updateOrdersForMealOptionOne(object sender, ValueChangedEventArgs e)
+        {
+            optionOneMealOrders = (int)e.NewValue;
+            optionOneLabel.Text = optionOneMealOrders.ToString();
+        }
+
+        private void reduceOrdersForMealOptionTwo(object sender, System.EventArgs e)
+        {
+            if (optionTwoMealOrders > 0)
+            {
+                optionTwoMealOrders = optionTwoMealOrders - 1;
+                optioneTwoSlider.Value = (double)optionTwoMealOrders;
+                optionTwoLabel.Text = optionTwoMealOrders.ToString();
+            }
+            else
+            {
+                optionTwoLabel.Text = optionTwoMealOrders.ToString();
+            }
+        }
+
+        private void addOrdersForMealOptionTwo(object sender, System.EventArgs e)
+        {
+            if (optionTwoMealOrders < 10)
+            {
+                optionTwoMealOrders = optionTwoMealOrders + 1;
+                optioneTwoSlider.Value = (double)optionTwoMealOrders;
+                optionTwoLabel.Text = optionTwoMealOrders.ToString();
+            }
+            else
+            {
+                optionTwoLabel.Text = optionTwoMealOrders.ToString();
+            }
+        }
+
+        private void updateOrdersForMealOptionTwo(object sender, ValueChangedEventArgs e)
+        {
+            optionTwoMealOrders = (int)e.NewValue;
+            optionTwoLabel.Text = optionTwoMealOrders.ToString();
         }
 
         //leaving this function here for the meal photo GET request
