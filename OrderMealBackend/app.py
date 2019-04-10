ff09e3fcd6dbe59be26f038c807c5a0eb1dabec2
Japan Parikh <parikh.japan30@gmail.com>
2019-04-09 21:32:53 -0700

    add new apis structure that needs to be worked on

--- OrderMealBackend/app.py
+++ OrderMealBackend/app.py
@@ -2,7 +2,7 @@
 # @Author: Japan Parikh
 # @Date:   2019-02-16 15:26:12
 # @Last Modified by:   Japan Parikh
-# @Last Modified time: 2019-03-14 19:25:12
+# @Last Modified time: 2019-04-09 21:31:19
 
 
 import os
@@ -19,7 +19,7 @@ from flask_mail import Mail, Message
 from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__, template_folder='assets')
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r'/api/*': {'origins': '*'}})
 
 app.config['MAIL_USERNAME'] = os.environ.get('EMAIL')
 app.config['MAIL_PASSWORD'] = os.environ.get('PASSWORD')
@@ -61,6 +61,21 @@ def upload_file(file, bucket, key):
         image_uploaded = True
 
     return image_uploaded
+
+
+def upload_meal_img(file, bucket, key):
+    if file and allowed_file(file.filename):
+        filename = 'https://s3-us-west-2.amazonaws.com/' \
+                   + str(bucket) + '/' + str(key)
+        upload_file = s3.put_object(
+                            Bucket=bucket,
+                            Body=file,
+                            Key=key,
+                            ACL='public-read',
+                            ContentType='image/jpeg'
+                        )
+        return filename
+    return None
 
 def allowed_file(filename):
     """Checks if the file is allowed to upload"""
@@ -169,7 +184,132 @@ class MealOrders(Resource):
             return response, 200
         except:
             raise BadRequest('Request failed. please try again later.')
-    
+
+
+class RegisterKitchen(Resource):
+    def post(self):
+        response = {}
+        data = request.get_json(force=True)
+        created_at = datetime.now(tz=timezone('US/Pacific')).strftime("%Y-%m-%dT%H:%M:%S")
+
+        if data.get('name') == None \
+          or data.get('description') == None \
+          or data.get('username') == None \
+          or data.get('password') == None \
+          or data.get('first_name') == None \
+          or data.get('last_name') == None \
+          or data.get('address') == None \
+          or data.get('city') == None \
+          or data.get('state') == None \
+          or data.get('zipcode') == None \
+          or data.get('phone_number') == None \
+          or data.get('close_time') == None \
+          or data.get('open_time') == None:
+            raise BadRequest('Request failed. Please provide all \
+                              required information.')
+
+        #TODO1: scan through the kitchen table and filter the results with the kitchen name 
+        #      used for signing up. Start by uncommenting the code below.
+
+        # kitchen = db.scan(TableName="")
+
+        # if kitchen.get('Items') != []:
+        #     response['message'] = 'This kitchen name is already taken.'
+        #     return response, 400
+
+        kitchen_id = uuid.uuid4().hex
+
+        try:
+            add_kitchen = db.put_item(TableName='kitchen',
+                Item={'id': {'S': kitchen_id},
+                      'created_at': {'S': created_at},
+                      'name': {'S': data['name']},
+                      'description': {'S': data['description']},
+                      'username': {'S': data['username']},
+                      'password': {'S': generate_password_hash(data['password'])},
+                      'first_name': {'S': data['first_name']},
+                      'last_name': {'S': data['last_name']},
+                      'address': {'S': data['address']},
+                      'city': {'S': data['city']},
+                      'state': {'S': data['state']},
+                      'zipcode': {'N': str(data['zipcode'])},
+                      'phone_number': {'S': str(data['phone_number'])},
+                      'open_time': {'S': str(data['open_time'])},
+                      'close_time': {'S': str(data['close_time'])}
+                      'isOpen': {'BOOL': False}
+                }
+            )
+
+            response['message'] = 'Request successful'
+            return response, 200
+        except:
+            raise BadRequest('Request failed. Please try again later.')
+
+
+class Kitchens(Resource):
+    def get(self):
+        """Returns all kitchens"""
+        response = {}
+
+        try:
+            #TODO1: use scan table method to scan through the kitchens table and
+            #      and use filterExpression to filter kitchens that are open today.
+            #      The key to filter kitchens that are open is "isOpen" and the value its BOOL
+
+            response['message'] = 'Request successful'
+            response['result'] = #TODO2: send the values of Items key from the response
+            return response, 200
+        except:
+            raise BadRequest('Request failed. Please try again later.')
+
+
+class Meals(Resource):
+    def post(self, kitchen_id):
+        response = {}
+
+        #TODO1: Go through the database schema and validate if the all the data
+        #       that is needed to post a meal is provided. If not, then raise a BadRequest Exception
+
+
+        #TODO2: create a unique id for the meal
+
+        try:
+            #TODO3: use put_item method to write data to the meals database table
+
+            response['message'] = 'Request successful'
+            return response, 201
+        except:
+            raise BadRequest('Request failed. Please try again later.')
+
+    def get(self, kitchen_id):
+        response = {}
+
+        try:
+            #TODO1: use scan or query method to scan through the meals table using the kitchen id
+            #      and use filterExpression to filter data using timestamp by today's date
+
+            response['message'] = 'Request successful'
+            response['result'] = #TODO2: send the value of Items key from the response
+            return response, 200
+        except:
+            raise BadRequest('Request failed. Please try again later.')
+
+
+class MealOrdersForKitchen(Resource):
+    def get(self, kitchen_id):
+        response = {}
+        data = request.get_json(force=True)
+
+        #TODO1: get todays date
+
+        try:
+            #TODO2: scan through the orders table and filter orders by the kitchen id and by today's date
+
+            response['message'] = 'Request successful!'
+            response['result'] = #TODO3: send the value of Items key from the response
+            return response, 200
+        except:
+            raise BadRequest('Request failed. Please try again later.')
 
 
 api.add_resource(MealOrders, '/api/v1/meal/order')
